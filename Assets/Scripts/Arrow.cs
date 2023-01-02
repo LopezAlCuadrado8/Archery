c2283ff5eee5f226317b0fc6f0b04c95687ee555
LopezAlCuadrado8 <118569776+LopezAlCuadrado8@users.noreply.github.com>
2023-01-02 12:25:31 -0500

    Mecanicas de Arco
    Modificacion en las mecanicas de la flecha y el disparo

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -1,26 +1,66 @@
+using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Arrow : MonoBehaviour
 {
-    [SerializeField] Rigidbody rb;
+    [SerializeField] float despawn = 10;
 
+    Rigidbody rb;
+    float rotacion;
+    bool flechaDisparada;
+
+    #region Metodos MonoBehaviour
     private void Start()
     {
-        Destroy(gameObject, 10);
+       rb = GetComponent<Rigidbody>();
+       Destroy(gameObject, despawn);
     }
 
+    private void Update()
+    {
+        SeguirArco();
+
+        if (rb.velocity.y < 0)
+        {
+            StartCoroutine(Torque(rotacion));
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag.Equals("piso"))
         {
-            rb.isKinematic = true;
+            //Stop Moving/Translating
+            rb.velocity = Vector3.zero;
+
+            //Stop rotating
+            rb.angularVelocity = Vector3.zero;
+
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+    #endregion
+
+    private void SeguirArco()
+    {
+        if (!flechaDisparada)
+        {
+            transform.position = transform.parent.position;
         }
     }
 
     public void Movimiento(float fuerza, float rotacion, Transform transformadaPadre)
     {
-        rb.AddForce(transformadaPadre.forward * fuerza, ForceMode.Impulse);
-        rb.AddTorque(transform.right * rotacion, ForceMode.Force);
+        this.rotacion = rotacion;
+        flechaDisparada = true;
+        rb.isKinematic = false;
+        rb.AddForce(transform.parent.forward * fuerza, ForceMode.Impulse);
+        transform.SetParent(null);
     }
 
+    IEnumerator Torque(float rotacion)
+    {
+        rb.AddTorque(transform.right * rotacion, ForceMode.Impulse);
+        yield return null;
+    }
 }
--- Assets/Scripts/ShootArrow.cs
+++ Assets/Scripts/ShootArrow.cs
@@ -5,26 +5,40 @@ public class ShootArrow : MonoBehaviour
     [SerializeField] GameObject flechaPrefab;
     [Space(5)]
     [SerializeField] float fuerzaMinima = 5;
-    [SerializeField] float rotacion = 0.3f;
+    [SerializeField] float torque = 0.09f;
 
     GameObject flecha;
     float fuerza;
+
+    Vector3 posicion;
+    Quaternion rotacion;
+
     private void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            Debug.Log("clicj");
-            flecha = Instantiate(flechaPrefab, transform.position, transform.rotation);       
-        }
+        ControlesDisparoFlecha();
+    }
 
+    private void ControlesDisparoFlecha()
+    {
         if (Input.GetMouseButtonDown(0))
         {
+            fuerza = fuerzaMinima;
+            posicion = transform.position;
+            rotacion = transform.GetChild(0).transform.rotation;
+            flecha = Instantiate(flechaPrefab, posicion, rotacion, transform.GetChild(0).transform);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log(fuerza);
             fuerza += 0.1f;
+            //Fuerza Maxima
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            flecha.GetComponent<Arrow>().Movimiento(fuerza, rotacion, transform);
+            flecha.GetComponent<Arrow>().Movimiento(fuerza, torque, transform);
+            fuerza = fuerzaMinima;
         }
     }
 }
